c888cd04af9501e382730e77f18810b1526a3111
Tazhimbetov Daniyar <d_tazhimbetov@kbtu.kz>
2024-01-20 15:19:58 +0600

    Booleans

---
+++ practice/week2/2.py
@@ -0,0 +1,7 @@
+a= 55
+b= 200
+
+if a>b:
+  print ("a is greater than b")
+else :
+  print ("b is greater than a")
---
+++ practice/week2/5.py
@@ -0,0 +1,4 @@
+x=10.45
+y=16
+print (isinstance (x, int))
+print (isinstance (y, int))
