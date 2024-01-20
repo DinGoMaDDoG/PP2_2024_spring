be054e827036b8aeec889ca577cd5d2c89d4966d
Tazhimbetov Daniyar <d_tazhimbetov@kbtu.kz>
2024-01-20 14:57:43 +0600

    switching folders

---
+++ practice/week1/6.py
@@ -0,0 +1,30 @@
+x=5
+y="John"
+print(x)
+print(y)
+x=4
+x="Sally"
+x=str (3)
+x=int (3)
+z=float (3)
+print (type(x))
+print (type(y))
+a = 4
+A = "Sally"
+#A will not overwrite a
+x, y, z="Orange", "Banana", "Cherry"
+print (x)
+print (y)
+print (z)
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+fruits = ["Orange","Banana", "Cherry"]
+x, y, z=fruits
+print (x)
+print (y)
+print (z)
+x=5
+y=10
+print (x+y)
---
+++ practice/week1/7.py
@@ -0,0 +1,30 @@
+x = 5
+print(type(x))
+x = "Hello World"	
+print (x)
+x = 20		
+print (x)
+x = 20.5
+print (x)		
+x = 1j	
+print (x)	
+x = ["apple", "banana", "cherry"]		
+print (x)
+x = ("apple", "banana", "cherry")		
+print (x)
+x = range(6)
+print (x)	
+x = {"name" : "John", "age" : 36}
+print (x)		
+x = {"apple", "banana", "cherry"}	
+print (x)
+x = frozenset({"apple", "banana", "cherry"})	
+print (x)	
+x = True		
+print (x)
+x = bytearray(5)	
+print (x)
+x = memoryview(bytes(5))	
+print (x)
+x = None
+print (x)
