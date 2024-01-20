147c703baf5dbef0ffbe91ebc6260b00b97db6df
Tazhimbetov Daniyar <d_tazhimbetov@kbtu.kz>
2024-01-20 14:43:13 +0600

    samples from 7-10

---
+++ lecture/week1/10.py
@@ -0,0 +1,65 @@
+print("Hello")
+print('Hello')
+
+a = "Hello"
+print(a)
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+a = "Hello, World!"
+print(a[1])
+
+for x in "banana":
+  print(x)
+
+a = "Hello, World!"
+print(len(a))
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+
+b="Hello, World!"
+print (b[2:5])
+print (b[:5])
+print (b[2:])
+print(b[-5:-2])
+print (b.upper())
+print (b.lower())
+a="   Hello, World!   "
+print (a.strip())       # !!!
+print (a.replace("H", "J"))
+print (b.split(","))
+a="Hello"
+b="World"
+print (a+" "+b)
+
+age=36
+txt="My name is John, I am {} years old"
+print (txt.format(age))
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price))
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price))
