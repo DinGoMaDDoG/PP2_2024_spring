292359f907f3cced8bb2f3994b9a1704cdf6201f
Tazhimbetov Daniyar <d_tazhimbetov@kbtu.kz>
2024-02-03 15:41:02 +0600

    functions2

--- practice/week3/function_4.py
+++ practice/week3/function_4.py
@@ -1,12 +1,14 @@
 def counter (x):
   result=0
-  for i in range (1, x):
+  for i in range (1, x+1 ):
     if (x % i ==0):
-      result+=result
+      result+=1
   return result
 
 def isPrime(i):
    if counter(i)==2:
+    print(i)
+   elif i==1:
     print(i)
 
 n=input() 
